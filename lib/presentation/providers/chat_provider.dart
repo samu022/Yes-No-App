7d37de43d210b6f4c921e75a5763bf5ff28ce0ef
samu022 <samuelalegre2002@gmail.com>
2025-06-22 18:34:54 -0400

    Mensajes de provider mostrar

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'Hola Samu', fromWho: FromWho.mine),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.mine),
   ];
