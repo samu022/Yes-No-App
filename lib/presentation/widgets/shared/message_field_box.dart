85e86a1647ddfc60ca1e8051f139d68548378e64
samu022 <samuelalegre2002@gmail.com>
2025-06-22 12:04:16 -0400

    focus en formfield

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -6,31 +6,42 @@ class MessageFieldBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //final colors= Theme.of(context).colorScheme;
-
+    final textController=TextEditingController();
+    final focusNode = FocusNode();
     final outlineInputBorder=UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent ),
         borderRadius: BorderRadius.circular(40)
         
     );
     final inputDecoration =InputDecoration(
+      hintText: 'End your message with a "??"',
         enabledBorder: outlineInputBorder,
         focusedBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
           onPressed: (){
-            print('Valor de la caja de texto');
+            final textValue = textController.value.text;
+            print('button $textValue');
+            textController.clear();
           },
         ),
       );
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('Submit value');
+        textController.clear();
+        focusNode.requestFocus();
       },
-      onChanged: (value) {
+      /*onChanged: (value) {
         print('changed: $value');
-      },
+      },*/
     );
   }
 }
